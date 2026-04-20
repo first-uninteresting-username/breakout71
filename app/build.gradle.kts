3efe37555037044b61219951e5ed818ec2f7f461
Renan LE CARO <renan@lecaro.me>
2026-04-20 22:08:58 +0200

    Build 29611928

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "me.lecaro.breakout"
         minSdk = 21
         targetSdk = 36
-        versionCode = 29608884
-        versionName = "29608884"
+        versionCode = 29611928
+        versionName = "29611928"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
--- src/PWA/sw-b71.js
+++ src/PWA/sw-b71.js
@@ -1,5 +1,5 @@
 // The version of the cache.
-const          VERSION = '29608884'
+const           VERSION = '29611928'
 
 // The name of the cache
 const CACHE_NAME = `breakout-71-${VERSION}`;
