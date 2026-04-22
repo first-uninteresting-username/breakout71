7bd8f4fe5a0367b1cfff93dae6cbc9e64e27ab9e
Renan LE CARO <renan@lecaro.me>
2026-04-22 19:10:03 +0200

    Build 29614629

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "me.lecaro.breakout"
         minSdk = 21
         targetSdk = 36
-        versionCode = 29613221
-        versionName = "29613221"
+        versionCode = 29614629
+        versionName = "29614629"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
