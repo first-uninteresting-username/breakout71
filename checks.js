82ed5e7462444d3798d378ba689c78a2a08630e6
Renan LE CARO <renan@lecaro.me>
2026-05-19 18:42:36 +0200

    fix: stop messing with "disco" of "disco ball"

--- checks.js
+++ checks.js
@@ -1,15 +1,20 @@
 // node checks.js
-const fs= require('fs')
-const files = fs.readdirSync('./src/i18n/')
-for(let filename of files){
-    if(!filename.endsWith('.json')) continue
-    const content = JSON.parse(fs.readFileSync(`./src/i18n/${filename}`))
-    for(let key in content){
-        if(content[key].match(   /<|>|http|\bpuck\b|\bpalet\b|퍽|\bdisco\b|шайба|冰球|rondelle/gi,)){
-            content[key]=''
-            console.log(`Removed ${key} of ${filename}`)
-        }
-        content[key]=content[key].trim()
+const fs = require("fs");
+const files = fs.readdirSync("./src/i18n/");
+for (let filename of files) {
+  if (!filename.endsWith(".json")) continue;
+  const content = JSON.parse(fs.readFileSync(`./src/i18n/${filename}`));
+  for (let key in content) {
+    if (
+      content[key].match(/<|>|http|\bpuck\b|\bpalet\b|퍽|шайба|冰球|rondelle/gi)
+    ) {
+      content[key] = "";
+      console.log(`Removed ${key} of ${filename}`);
     }
-    fs.writeFileSync(`./src/i18n/${filename}`, JSON.stringify(content, null,4)+'\n')
+    content[key] = content[key].trim();
+  }
+  fs.writeFileSync(
+    `./src/i18n/${filename}`,
+    JSON.stringify(content, null, 4) + "\n",
+  );
 }
