0f30da8ca03e08005fc51e0411e4ac607fa69af5
Renan LE CARO <renan@lecaro.me>
2026-07-25 07:34:33 +0200

    Build 29749294

--- src/PWA/sw-b71.js
+++ src/PWA/sw-b71.js
@@ -1,5 +1,5 @@
 // The version of the cache.
-const                                                                                        VERSION = '29739371'
+const                                                                                         VERSION = '29749294'
 
 // The name of the cache
 const CACHE_NAME = `breakout-71-${VERSION}`;
